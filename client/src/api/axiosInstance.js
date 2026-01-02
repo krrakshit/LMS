53447c9006ff2c101b22e840455cb1352d949e43
RAKSHIT <157181111+krrakshit@users.noreply.github.com>
2026-01-02 17:50:19 +0530

    Deploy

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://stratum.rexit.live",
 });
 
 axiosInstance.interceptors.request.use(
